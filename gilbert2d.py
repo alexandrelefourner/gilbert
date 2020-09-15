81bcfb97f09d0cc28f0bfbe15c99b721f6fc7cf7
Alexandrelf <alexandre.lefourner@gmail.com>
2020-09-15 17:31:43 +0300

    Code modification for numpy transformation

--- gilbert2d.py
+++ gilbert2d.py
@@ -1,6 +1,10 @@
-#!/usr/bin/env python
+"""
+Original code from Jakub Červený.
+https://github.com/jakubcerveny/gilbert
+Adapted by Alexandre A. LE FOURNER to get a numpy array.
+"""
 
-import sys
+import numpy as np
 
 def sgn(x):
     return (x > 0) - (x < 0)
@@ -12,25 +16,26 @@ def gilbert2d(x, y, ax, ay, bx, by):
     2D rectangular grids.
     """
 
-    w = abs(ax + ay)
-    h = abs(bx + by)
+    w = int(abs(ax + ay))
+    h = int(abs(bx + by))
 
     (dax, day) = (sgn(ax), sgn(ay)) # unit major direction
     (dbx, dby) = (sgn(bx), sgn(by)) # unit orthogonal direction
 
+    mlist = []
     if h == 1:
         # trivial row fill
         for i in range(0, w):
-            print x, y
+            mlist.append((int(x), int(y)))
             (x, y) = (x + dax, y + day)
-        return
+        return mlist
 
     if w == 1:
         # trivial column fill
         for i in range(0, h):
-            print x, y
+            mlist.append((int(x), int(y)))
             (x, y) = (x + dbx, y + dby)
-        return
+        return mlist
 
     (ax2, ay2) = (ax/2, ay/2)
     (bx2, by2) = (bx/2, by/2)
@@ -44,8 +49,8 @@ def gilbert2d(x, y, ax, ay, bx, by):
             (ax2, ay2) = (ax2 + dax, ay2 + day)
 
         # long case: split in two parts only
-        gilbert2d(x, y, ax2, ay2, bx, by)
-        gilbert2d(x+ax2, y+ay2, ax-ax2, ay-ay2, bx, by)
+        mlist = mlist + gilbert2d(x, y, ax2, ay2, bx, by)
+        mlist = mlist + gilbert2d(x+ax2, y+ay2, ax-ax2, ay-ay2, bx, by)
 
     else:
         if (h2 % 2) and (h > 2):
@@ -53,21 +58,18 @@ def gilbert2d(x, y, ax, ay, bx, by):
             (bx2, by2) = (bx2 + dbx, by2 + dby)
 
         # standard case: one step up, one long horizontal, one step down
-        gilbert2d(x, y, bx2, by2, ax2, ay2)
-        gilbert2d(x+bx2, y+by2, ax, ay, bx-bx2, by-by2)
-        gilbert2d(x+(ax-dax)+(bx2-dbx), y+(ay-day)+(by2-dby),
+        mlist = mlist + gilbert2d(x, y, bx2, by2, ax2, ay2)
+        mlist = mlist + gilbert2d(x+bx2, y+by2, ax, ay, bx-bx2, by-by2)
+        mlist = mlist + gilbert2d(x+(ax-dax)+(bx2-dbx), y+(ay-day)+(by2-dby),
                  -bx2, -by2, -(ax-ax2), -(ay-ay2))
+    return mlist
 
-
-def main():
-    width = int(sys.argv[1])
-    height = int(sys.argv[2])
-
+def numpy_gilbert2d(width, height):
+    """
+    returns a numpy array of girlbert for easy data transformation.
+    """
+	
     if width >= height:
-        gilbert2d(0, 0, width, 0, 0, height)
+        return np.array(gilbert2d(0, 0, width, 0, 0, height))
     else:
-        gilbert2d(0, 0, 0, height, width, 0)
-
-
-if __name__ == "__main__":
-    main()
+        return np.array(gilbert2d(0, 0, 0, height, width, 0))
--- gilbert3d.py
+++ gilbert3d.py
@@ -1,23 +1,28 @@
-#!/usr/bin/env python
-
-import sys
+"""
+Original code from Jakub Červený.
+https://github.com/jakubcerveny/gilbert
+Adapted by Alexandre A. LE FOURNER to get a numpy array with Python 3
+"""
+import numpy as np
 
 def sgn(x):
     return (x > 0) - (x < 0)
 
 
 def gilbert3d(x, y, z,
-              ax, ay, az,
-              bx, by, bz,
-              cx, cy, cz):
+                ax, ay, az,
+                bx, by, bz,
+                cx, cy, cz):
     """
     Generalized Hilbert ('Gilbert') space-filling curve for arbitrary-sized
     3D rectangular grids.
     """
 
-    w = abs(ax + ay + az)
-    h = abs(bx + by + bz)
-    d = abs(cx + cy + cz)
+    w = int(abs(ax + ay + az))
+    h = int(abs(bx + by + bz))
+    d = int(abs(cx + cy + cz))
+
+    mlist = []
 
     (dax, day, daz) = (sgn(ax), sgn(ay), sgn(az)) # unit major direction ("right")
     (dbx, dby, dbz) = (sgn(bx), sgn(by), sgn(bz)) # unit ortho direction ("forward")
@@ -26,21 +31,21 @@ def gilbert3d(x, y, z,
     # trivial row/column fills
     if h == 1 and d == 1:
         for i in range(0, w):
-            print x, y, z
+            mlist.append((int(x), int(y), int(z)))
             (x, y, z) = (x + dax, y + day, z + daz)
-        return
+        return mlist
 
     if w == 1 and d == 1:
         for i in range(0, h):
-            print x, y, z
+            mlist.append((int(x), int(y), int(z)))
             (x, y, z) = (x + dbx, y + dby, z + dbz)
-        return
+        return mlist
 
     if w == 1 and h == 1:
         for i in range(0, d):
-            print x, y, z
+            mlist.append((int(x), int(y), int(z)))
             (x, y, z) = (x + dcx, y + dcy, z + dcz)
-        return
+        return mlist
 
     (ax2, ay2, az2) = (ax/2, ay/2, az/2)
     (bx2, by2, bz2) = (bx/2, by/2, bz/2)
@@ -52,121 +57,117 @@ def gilbert3d(x, y, z,
 
     # prefer even steps
     if (w2 % 2) and (w > 2):
-       (ax2, ay2, az2) = (ax2 + dax, ay2 + day, az2 + daz)
+            (ax2, ay2, az2) = (ax2 + dax, ay2 + day, az2 + daz)
 
     if (h2 % 2) and (h > 2):
-       (bx2, by2, bz2) = (bx2 + dbx, by2 + dby, bz2 + dbz)
+            (bx2, by2, bz2) = (bx2 + dbx, by2 + dby, bz2 + dbz)
 
     if (d2 % 2) and (d > 2):
-       (cx2, cy2, cz2) = (cx2 + dcx, cy2 + dcy, cz2 + dcz)
+            (cx2, cy2, cz2) = (cx2 + dcx, cy2 + dcy, cz2 + dcz)
 
     # wide case, split in w only
     if (2*w > 3*h) and (2*w > 3*d):
-       gilbert3d(x, y, z,
-                 ax2, ay2, az2,
-                 bx, by, bz,
-                 cx, cy, cz)
+            mlist = mlist + gilbert3d(x, y, z,
+                ax2, ay2, az2,
+                bx, by, bz,
+                cx, cy, cz)
 
-       gilbert3d(x+ax2, y+ay2, z+az2,
-                 ax-ax2, ay-ay2, az-az2,
-                 bx, by, bz,
-                 cx, cy, cz)
+            mlist = mlist + gilbert3d(x+ax2, y+ay2, z+az2,
+                ax-ax2, ay-ay2, az-az2,
+                bx, by, bz,
+                cx, cy, cz)
 
     # do not split in d
     elif 3*h > 4*d:
-       gilbert3d(x, y, z,
-                 bx2, by2, bz2,
-                 cx, cy, cz,
-                 ax2, ay2, az2)
+            mlist = mlist + gilbert3d(x, y, z,
+                bx2, by2, bz2,
+                cx, cy, cz,
+                ax2, ay2, az2)
 
-       gilbert3d(x+bx2, y+by2, z+bz2,
-                 ax, ay, az,
-                 bx-bx2, by-by2, bz-bz2,
-                 cx, cy, cz)
+            mlist = mlist + gilbert3d(x+bx2, y+by2, z+bz2,
+                ax, ay, az,
+                bx-bx2, by-by2, bz-bz2,
+                cx, cy, cz)
 
-       gilbert3d(x+(ax-dax)+(bx2-dbx),
-                 y+(ay-day)+(by2-dby),
-                 z+(az-daz)+(bz2-dbz),
-                 -bx2, -by2, -bz2,
-                 cx, cy, cz,
-                 -(ax-ax2), -(ay-ay2), -(az-az2))
+            mlist = mlist + gilbert3d(x+(ax-dax)+(bx2-dbx),
+                y+(ay-day)+(by2-dby),
+                z+(az-daz)+(bz2-dbz),
+                -bx2, -by2, -bz2,
+                cx, cy, cz,
+                -(ax-ax2), -(ay-ay2), -(az-az2))
 
     # do not split in h
     elif 3*d > 4*h:
-       gilbert3d(x, y, z,
-                 cx2, cy2, cz2,
-                 ax2, ay2, az2,
-                 bx, by, bz)
+            mlist = mlist + gilbert3d(x, y, z,
+                cx2, cy2, cz2,
+                ax2, ay2, az2,
+                bx, by, bz)
 
-       gilbert3d(x+cx2, y+cy2, z+cz2,
-                 ax, ay, az,
-                 bx, by, bz,
-                 cx-cx2, cy-cy2, cz-cz2)
+            mlist = mlist + gilbert3d(x+cx2, y+cy2, z+cz2,
+                ax, ay, az,
+                bx, by, bz,
+                cx-cx2, cy-cy2, cz-cz2)
 
-       gilbert3d(x+(ax-dax)+(cx2-dcx),
-                 y+(ay-day)+(cy2-dcy),
-                 z+(az-daz)+(cz2-dcz),
-                 -cx2, -cy2, -cz2,
-                 -(ax-ax2), -(ay-ay2), -(az-az2),
-                 bx, by, bz)
+            mlist = mlist + gilbert3d(x+(ax-dax)+(cx2-dcx),
+                y+(ay-day)+(cy2-dcy),
+                z+(az-daz)+(cz2-dcz),
+                -cx2, -cy2, -cz2,
+                -(ax-ax2), -(ay-ay2), -(az-az2),
+                bx, by, bz)
 
     # regular case, split in all w/h/d
     else:
-       gilbert3d(x, y, z,
-                 bx2, by2, bz2,
-                 cx2, cy2, cz2,
-                 ax2, ay2, az2)
+            mlist = mlist + gilbert3d(x, y, z,
+                bx2, by2, bz2,
+                cx2, cy2, cz2,
+                ax2, ay2, az2)
 
-       gilbert3d(x+bx2, y+by2, z+bz2,
-                 cx, cy, cz,
-                 ax2, ay2, az2,
-                 bx-bx2, by-by2, bz-bz2)
+            mlist = mlist + gilbert3d(x+bx2, y+by2, z+bz2,
+                cx, cy, cz,
+                ax2, ay2, az2,
+                bx-bx2, by-by2, bz-bz2)
 
-       gilbert3d(x+(bx2-dbx)+(cx-dcx),
-                 y+(by2-dby)+(cy-dcy),
-                 z+(bz2-dbz)+(cz-dcz),
-                 ax, ay, az,
-                 -bx2, -by2, -bz2,
-                 -(cx-cx2), -(cy-cy2), -(cz-cz2))
+            mlist = mlist + gilbert3d(x+(bx2-dbx)+(cx-dcx),
+                y+(by2-dby)+(cy-dcy),
+                z+(bz2-dbz)+(cz-dcz),
+                ax, ay, az,
+                -bx2, -by2, -bz2,
+                -(cx-cx2), -(cy-cy2), -(cz-cz2))
 
-       gilbert3d(x+(ax-dax)+bx2+(cx-dcx),
-                 y+(ay-day)+by2+(cy-dcy),
-                 z+(az-daz)+bz2+(cz-dcz),
-                 -cx, -cy, -cz,
-                 -(ax-ax2), -(ay-ay2), -(az-az2),
-                 bx-bx2, by-by2, bz-bz2)
+            mlist = mlist + gilbert3d(x+(ax-dax)+bx2+(cx-dcx),
+                y+(ay-day)+by2+(cy-dcy),
+                z+(az-daz)+bz2+(cz-dcz),
+                -cx, -cy, -cz,
+                -(ax-ax2), -(ay-ay2), -(az-az2),
+                bx-bx2, by-by2, bz-bz2)
 
-       gilbert3d(x+(ax-dax)+(bx2-dbx),
-                 y+(ay-day)+(by2-dby),
-                 z+(az-daz)+(bz2-dbz),
-                 -bx2, -by2, -bz2,
-                 cx2, cy2, cz2,
-                 -(ax-ax2), -(ay-ay2), -(az-az2))
+            mlist = mlist + gilbert3d(x+(ax-dax)+(bx2-dbx),
+                y+(ay-day)+(by2-dby),
+                z+(az-daz)+(bz2-dbz),
+                -bx2, -by2, -bz2,
+                cx2, cy2, cz2,
+                -(ax-ax2), -(ay-ay2), -(az-az2))
+    return mlist
 
 
-def main():
-    width = int(sys.argv[1])
-    height = int(sys.argv[2])
-    depth = int(sys.argv[3])
-
+def numpy_gilbert3d(width, height, depth):
+    """
+    returns a numpy array of girlbert for easy data transformation.
+    """
     if width >= height and width >= depth:
-       gilbert3d(0, 0, 0,
-                 width, 0, 0,
-                 0, height, 0,
-                 0, 0, depth)
+            return np.array(gilbert3d(0, 0, 0,
+                width, 0, 0,
+                0, height, 0,
+                0, 0, depth))
 
     elif height >= width and height >= depth:
-       gilbert3d(0, 0, 0,
-                 0, height, 0,
-                 width, 0, 0,
-                 0, 0, depth)
+            return np.array(gilbert3d(0, 0, 0,
+                0, height, 0,
+                width, 0, 0,
+                0, 0, depth))
 
     else: # depth >= width and depth >= height
-       gilbert3d(0, 0, 0,
-                 0, 0, depth,
-                 width, 0, 0,
-                 0, height, 0)
-
-
-if __name__ == "__main__":
-    main()
+            return np.array(gilbert3d(0, 0, 0,
+                0, 0, depth,
+                width, 0, 0,
+                0, height, 0))
